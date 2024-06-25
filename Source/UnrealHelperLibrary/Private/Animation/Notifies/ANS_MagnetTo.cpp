ac6e9033df655b9940e8ceebd88e669e373c096b
Ciberus <ciberus.ps@gmail.com>
2024-06-25 14:34:00 +0300

    draft ANS_MagnetTo

---
+++ Source/UnrealHelperLibrary/Private/Animation/Notifies/ANS_MagnetTo.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/Notifies/ANS_MagnetTo.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UANS_MagnetTo::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+    Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+    // Speed = Distance / TotalDuration / 60;
+    BaseCharacter = Cast<ACharacter>(MeshComp->GetOwner());
+
+    FTimerHandle TimerHandle;
+    MeshComp->GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UANS_MagnetTo::TimerTick, 0.0f, true, -1);
+
+    // GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ABaseDestructibleActor::FinalDestruct, TimeToDisappear);
+}
+
+void UANS_MagnetTo::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+    Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+
+    Alpha = FMath::Clamp(Alpha + FrameDeltaTime, 0, 1);
+    FVector NewLocation = BaseCharacter->GetActorLocation();
+    float Delta = UKismetMathLibrary::Ease(0, Distance / 1000, Alpha, EEasingFunc::Linear);
+    NewLocation += BaseCharacter->GetActorForwardVector() * Delta;
+    BaseCharacter->SetActorLocation(NewLocation, true);
+    // BaseCharacter.Get()->GetCharacterMovement()->AddForce(BaseCharacter-> GetActorForwardVector() * Distance);
+}
+
+void UANS_MagnetTo::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+    Super::NotifyEnd(MeshComp, Animation, EventReference);
+}
+
+void UANS_MagnetTo::TimerTick()
+{
+    float test = 0.0f;
+}
+
