c479cb9d5f0538e28430bc738a93784b9b334d9f
Ciberus <ciberus.ps@gmail.com>
2024-09-16 09:37:40 +0300

    save

--- Source/UnrealHelperLibrary/Public/Subsystems/DebugSubsystem/AA_WaitDebugCategoryChange.h
+++ Source/UnrealHelperLibrary/Public/Subsystems/DebugSubsystem/AA_WaitDebugCategoryChange.h
@@ -3,49 +3,46 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UHLDebugSubsystem.h"
-#include "Engine/CancellableAsyncAction.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
 #include "AA_WaitDebugCategoryChange.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FWaitDebugCategoryChanged, bool, bEnabled);
 
 /**
  * TODO test that it works after several DebugCategory changes
  */
-UCLASS()
-class UNREALHELPERLIBRARY_API UAA_WaitDebugCategoryChange : public UCancellableAsyncAction
+UCLASS(BlueprintType, meta=(ExposedAsyncProxy = AsyncAction))
+class UNREALHELPERLIBRARY_API UAA_WaitDebugCategoryChange : public UBlueprintAsyncActionBase
 {
 	GENERATED_BODY()
 
 public:
-    /**
-     * Start a delay.
-     * @param DelayTime		How long the delay will last.
-     * @param WorldContext	Object from which the world will be derived.
-     */
-    UFUNCTION(BlueprintCallable, DisplayName="WaitDebugCategoryChange", meta=(WorldContext="WorldContext", BlueprintInternalUseOnly="true"))
-    static UAA_WaitDebugCategoryChange* WaitDebugCategoryChange(const UObject* WorldContext, FGameplayTag DebugCategory);
+    // TODO add updating bool value from Blueprint by reference
+    // UFUNCTION(BlueprintCallable, DisplayName="WaitDebugCategoryChange", meta=(WorldContext="WorldContext", BlueprintInternalUseOnly="true", AutoCreateRefTerm="bIsDebugEnabledValueRef", AdvancedDisplay="bIsDebugEnabledValueRef"))
+    // static UAA_WaitDebugCategoryChange* WaitDebugCategoryChange(const UObject* WorldContext, FGameplayTag DebugCategoryTag, bool bCheckOnStart, const bool& bIsDebugEnabledValueRef);
+    UFUNCTION(BlueprintCallable, Category="UnrealHelperLibrary", meta=(WorldContext="WorldContext", BlueprintInternalUseOnly="true", DefaultToSelf="WaitDebugCategoryChange", Keywords = "UnrealHelperLibrary debug UHL debugCategory debugging"))
+    static UAA_WaitDebugCategoryChange* WaitDebugCategoryChange(const UObject* WorldContext, FGameplayTag DebugCategoryTag, bool bCheckOnStart = true);
 
     UPROPERTY(BlueprintAssignable)
-    FOnUHLDebugCategoryChanged OnChange;
+    FWaitDebugCategoryChanged OnChange;
     UPROPERTY(BlueprintAssignable)
-    FOnUHLDebugCategoryChanged OnEnabled;
+    FWaitDebugCategoryChanged OnEnabled;
     UPROPERTY(BlueprintAssignable)
-    FOnUHLDebugCategoryChanged OnDisabled;
+    FWaitDebugCategoryChanged OnDisabled;
 
     virtual void Activate() override;
-    virtual void Cancel() override;
 
-    // Start UObject Functions
     virtual UWorld* GetWorld() const override
     {
         return ContextWorld.IsValid() ? ContextWorld.Get() : nullptr;
     }
-    // End UObject Functions
 
 private:
-    /** The context world of this action. */
     TWeakObjectPtr<UWorld> ContextWorld = nullptr;
 
-    FGameplayTag DebugCategory = FGameplayTag::EmptyTag;
+    FGameplayTag DebugCategoryTagInternal = FGameplayTag::EmptyTag;
+    bool bCheckOnStart = true;
 
     UFUNCTION()
     void OnDebugCategoryChange(FGameplayTag DebugCategoryTag, bool bEnabled);
