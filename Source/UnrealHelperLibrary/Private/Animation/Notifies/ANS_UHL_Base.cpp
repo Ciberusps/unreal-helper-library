e0a94a8a8086f61b541b3ed6b525c768b369e308
Ciberus <ciberus.ps@gmail.com>
2024-10-13 21:05:12 +0300

    rename ANS_UnrealHelperLibrary to ANS_UHL_Base

---
+++ Source/UnrealHelperLibrary/Private/Animation/Notifies/ANS_UHL_Base.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/Notifies/ANS_UHL_Base.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ANS_UHL_Base)
+
+void UANS_UHL_Base::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	CurrentAnimMontage = EventReference.GetNotify()->GetLinkedMontage();
+
+	if (CurrentAnimMontage.IsValid())
+	{
+		if (bUseOnMontageBlendingOut)
+		{
+			MeshComp->AnimScriptInstance->OnMontageBlendingOut.AddUniqueDynamic(this, &UANS_UHL_Base::OnMontageBlendingOut);
+		}
+	}
+}
+
+void UANS_UHL_Base::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+	if (bUseOnMontageBlendingOut)
+	{
+		MeshComp->AnimScriptInstance->OnMontageBlendingOut.RemoveDynamic(this, &UANS_UHL_Base::OnMontageBlendingOut);
+	}
+}
+
+void UANS_UHL_Base::OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (!CurrentAnimMontage.IsValid()
+		|| Montage != CurrentAnimMontage)
+	{
+		return;
+	}
+}
--- Source/UnrealHelperLibrary/Public/Animation/Notifies/ANS_UHL_Base.h
+++ Source/UnrealHelperLibrary/Public/Animation/Notifies/ANS_UHL_Base.h
@@ -4,14 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotifyState.h"
-#include "ANS_UnrealHelperLibrary.generated.h"
+#include "ANS_UHL_Base.generated.h"
 
 /**
  * with events like OnMontageBlendOut, OnMontageInterrupted...
  * you can disable subscribing OnMontageBlendOut, by "bUseOnMontageBlendingOut=false" in constructor
  */
 UCLASS()
-class UNREALHELPERLIBRARY_API UANS_UnrealHelperLibrary : public UAnimNotifyState
+class UNREALHELPERLIBRARY_API UANS_UHL_Base : public UAnimNotifyState
 {
 	GENERATED_BODY()
 
@@ -21,7 +21,7 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
 	bool bUseOnMontageBlendingOut = true;
 	TWeakObjectPtr<const UAnimMontage> CurrentAnimMontage;
-	
+
 	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
 	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
 
