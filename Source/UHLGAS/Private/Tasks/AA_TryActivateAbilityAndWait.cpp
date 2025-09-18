5a40339c63f05ace5525f3faf4667c06fafe0787
Ingarnm <121302342+Ingarnm@users.noreply.github.com>
2025-09-18 20:53:31 +0300

    [Dev] Update TryActivateAbilityAndWait task

--- Source/UHLGAS/Private/Tasks/AA_TryActivateAbilityAndWait.cpp
+++ Source/UHLGAS/Private/Tasks/AA_TryActivateAbilityAndWait.cpp
@@ -1,98 +1,65 @@
 // Pavel Penkov 2025 All Rights Reserved.
 
 
+
+
 #include "Tasks/AA_TryActivateAbilityAndWait.h"
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "UHLGASBlueprintLibrary.h"
-#include "Abilities/GameplayAbility.h"
-#include "Data/PayloadWithInstancedStructs.h"
-#include "Utils/UnrealHelperLibraryBPL.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(AA_TryActivateAbilityAndWait)
 
 void UAA_TryActivateAbilityAndWait::OnAbilityActivate(UGameplayAbility* ActivatedAbility)
 {
-	if (!IncludeTriggeredAbilities && ActivatedAbility->IsTriggered())
+	if (const UAbilitySystemComponent* ASC = GetAbilitySystemComponent())
 	{
-		return;
+		if (TrackedAbilitySpecHandle.IsValid())
+		{
+			if (FGameplayAbilitySpec* Spec = ASC->FindAbilitySpecFromHandle(TrackedAbilitySpecHandle))
+			{
+				if (Spec->GetPrimaryInstance() == ActivatedAbility)
+				{
+					if (ShouldBroadcastDelegates())
+					{
+						OnActivate.Broadcast(ActivatedAbility);
+					}	
+				}
+			}
+		}
 	}
-
-	const FGameplayTagContainer& AbilityTags = ActivatedAbility->GetAssetTags();
-	if (WithTag.IsValid() && !AbilityTags.HasTag(WithTag))
-	{
-		// Failed tag check
-		return;
-	}
-
-	if (ShouldBroadcastDelegates())
-	{
-		OnActivate.Broadcast(ActivatedAbility);
-	}
-
-	// if (TriggerOnce)
-	// {
-	// 	EndAction();
-	// }
 }
 
 void UAA_TryActivateAbilityAndWait::OnAbilityDeactivate(UGameplayAbility* ActivatedAbility)
 {
-	const FGameplayTagContainer& AbilityTags = ActivatedAbility->GetAssetTags();
-	if (WithTag.IsValid() && !AbilityTags.HasTag(WithTag))
+	if (const UAbilitySystemComponent* ASC = GetAbilitySystemComponent())
 	{
-		// Failed tag check
-		return;
-	}
-
-	if (ShouldBroadcastDelegates())
-	{
-		OnEndAbility.Broadcast(ActivatedAbility);
-	}
-
-	if (TriggerOnce)
-	{
-		EndAction();
+		if (TrackedAbilitySpecHandle.IsValid())
+		{
+			if (FGameplayAbilitySpec* Spec = ASC->FindAbilitySpecFromHandle(TrackedAbilitySpecHandle))
+			{
+				if (Spec->GetPrimaryInstance() == ActivatedAbility)
+				{
+					if (ShouldBroadcastDelegates())
+					{
+						OnEndAbility.Broadcast(ActivatedAbility);
+						EndAction();
+					}	
+				}
+			}
+		}
 	}
 }
 
-UAA_TryActivateAbilityAndWait* UAA_TryActivateAbilityAndWait::TryActivateAbilityAndWait(AActor* TargetActor,
-	FGameplayTag InWithTag, bool bUsingEvent_In, const TArray<FInstancedStruct>& PayloadInfo, bool InIncludeTriggeredAbilities, bool bAllowRemoteActivation, bool InTriggerOnce)
+
+UAA_TryActivateAbilityAndWait* UAA_TryActivateAbilityAndWait::TryActivateAbilityAndWait(AActor* TargetActor, FGameplayTagQuery TagQuery, bool bAllowRemoteActivation)
 {
 	UAA_TryActivateAbilityAndWait* MyObj = NewObject<UAA_TryActivateAbilityAndWait>();
 	MyObj->SetAbilityActor(TargetActor);
-	MyObj->WithTag = InWithTag;
-	MyObj->bUsingEvent = bUsingEvent_In;
-	MyObj->IncludeTriggeredAbilities = InIncludeTriggeredAbilities;
-	MyObj->TriggerOnce = InTriggerOnce;
+	MyObj->TagQuery = TagQuery;
 	MyObj->bAllowRemoteActivation = bAllowRemoteActivation;
-	MyObj->PayloadInfo = PayloadInfo;
 	return MyObj;
 }
-
-// UAA_TryActivateAbilityAndWait* UAA_TryActivateAbilityAndWait::WaitForAbilityDeactivateWithTagRequirements(
-// 	AActor* TargetActor, FGameplayTagRequirements TagRequirements, bool InIncludeTriggeredAbilities,
-// 	bool InTriggerOnce)
-// {
-// 	UAA_TryActivateAbilityAndWait* MyObj = NewObject<UAA_TryActivateAbilityAndWait>();
-// 	MyObj->SetAbilityActor(TargetActor);
-// 	MyObj->TagRequirements = TagRequirements;
-// 	MyObj->IncludeTriggeredAbilities = InIncludeTriggeredAbilities;
-// 	MyObj->TriggerOnce = InTriggerOnce;
-// 	return MyObj;
-// }
-//
-// UAA_TryActivateAbilityAndWait* UAA_TryActivateAbilityAndWait::WaitForAbilityDeactivate_Query(
-// 	AActor* TargetActor, FGameplayTagQuery Query, bool InIncludeTriggeredAbilities, bool InTriggerOnce)
-// {
-// 	UAA_TryActivateAbilityAndWait* MyObj = NewObject<UAA_TryActivateAbilityAndWait>();
-// 	MyObj->SetAbilityActor(TargetActor);
-// 	MyObj->Query = Query;
-// 	MyObj->IncludeTriggeredAbilities = InIncludeTriggeredAbilities;
-// 	MyObj->TriggerOnce = InTriggerOnce;
-// 	return MyObj;
-// }
 
 void UAA_TryActivateAbilityAndWait::Activate()
 {
@@ -100,30 +67,47 @@ void UAA_TryActivateAbilityAndWait::Activate()
 
 	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponent())
 	{
+		//@TODO: Оба делегата были в одном хендле, мб вернуть?
 		OnAbilityActivateDelegateHandle = ASC->AbilityActivatedCallbacks.AddUObject(this, &UAA_TryActivateAbilityAndWait::OnAbilityActivate);
-		OnAbilityActivateDelegateHandle = ASC->AbilityEndedCallbacks.AddUObject(this, &UAA_TryActivateAbilityAndWait::OnAbilityDeactivate);
+		OnAbilityDeactivateDelegateHandle = ASC->AbilityEndedCallbacks.AddUObject(this, &UAA_TryActivateAbilityAndWait::OnAbilityDeactivate);
 
 		bool bActivated = false;
-		if (bUsingEvent)
 		{
-			UPayloadWithInstancedStructs* Payload = NewObject<UPayloadWithInstancedStructs>();
-			Payload->InstancedStructs = PayloadInfo;
-			FGameplayEventData EventData;
-			EventData.OptionalObject = Payload;
-			int32 ActivatedAbilitiesCount = ASC->HandleGameplayEvent(WithTag, &EventData);
-			bActivated = (ActivatedAbilitiesCount > 0);
-		}
-		else
-		{
-			bActivated = UUHLGASBlueprintLibrary::TryActivateAbilityWithTag(ASC, WithTag, bAllowRemoteActivation);
+			TArray<FGameplayAbilitySpecHandle> OutAbilityHandles;
+			ASC->FindAllAbilitiesMatchingQuery(OutAbilityHandles, TagQuery);
+
+			if (OutAbilityHandles.Num() > 0)
+			{
+				int32 Index = 0;
+				while (Index < OutAbilityHandles.Num() && !bActivated)
+				{
+					FGameplayAbilitySpecHandle AbilitySpecHandle = OutAbilityHandles[Index];
+					if (AbilitySpecHandle.IsValid())
+					{
+						TrackedAbilitySpecHandle = AbilitySpecHandle;  
+						bActivated = ASC->TryActivateAbility(TrackedAbilitySpecHandle, bAllowRemoteActivation);
+					}
+					Index++;
+				}
+			}
 		}
 		if (!bActivated)
 		{
+			//If ability dint activated
+			if (ShouldBroadcastDelegates())
+			{
+				OnActivationFailed.Broadcast();
+			}
 			EndAction();
 		}
 	}
 	else
 	{
+		//If ability dint activated
+		if (ShouldBroadcastDelegates())
+		{
+			OnActivationFailed.Broadcast();
+		}
 		EndAction();
 	}
 }
@@ -133,7 +117,10 @@ void UAA_TryActivateAbilityAndWait::EndAction()
 	if (UAbilitySystemComponent* ASC = GetAbilitySystemComponent())
 	{
 		ASC->AbilityActivatedCallbacks.Remove(OnAbilityActivateDelegateHandle);
+		ASC->AbilityActivatedCallbacks.Remove(OnAbilityDeactivateDelegateHandle);
 	}
 	Super::EndAction();
 }
+
+
 
